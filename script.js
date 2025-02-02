e7577bb0684953c90c097abf25beeb83b86e098e
kalguzman <jamalmurray0010@gmail.com>
2025-02-02 12:22:58 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById("yesButton").addEventListener("click", function () {
-    alert("This is your official invitation to be my Valentine's date. \n\nMake sure to bring this invitation on the day itself. 💖\n\nWe had a very hectic semester, and I would like to make this break memorable and fun by spending a day together. ❤️");
+    document.getElementById("message").style.display = "block";
 });
 
 document.getElementById("noButton").addEventListener("mouseover", function () {
@@ -10,7 +10,6 @@ document.getElementById("noButton").addEventListener("mouseover", function () {
     let randomX = Math.floor(Math.random() * maxX);
     let randomY = Math.floor(Math.random() * maxY);
     
-    button.style.position = "absolute";
     button.style.left = randomX + "px";
     button.style.top = randomY + "px";
 });
